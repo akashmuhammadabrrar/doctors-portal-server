181f097b7a7bf7019fddb5fe0187d9fb0929ab0d
AkashAbrrar <mabrrar9@gmail.com>
2022-05-16 13:24:46 +0600

    booking slots available is done

--- index.js
+++ index.js
@@ -27,11 +27,36 @@ async function run() {
             res.send(services)
         });
 
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 15, 2022';
+
+            // step-1: get all services
+            const services = await servicesCollection.find().toArray();
+
+            // step-2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //step-3:  for each service find bookings for that service
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+
+            res.send(services);
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
             const result = await bookingCollection.insertOne(booking);
-            res.send(result);
+            return res.send({ success: true, result });
         })
 
     }
