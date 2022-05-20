58ebc70439a2315bf2dd2611f6a5d54b3d86c1bf
AkashAbrrar <mabrrar9@gmail.com>
2022-05-20 15:32:08 +0600

    this commit for add doctor to the database

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ async function run() {
         const servicesCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = servicesCollection.find(query);
+            const cursor = servicesCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services)
         });
@@ -119,7 +120,7 @@ async function run() {
             else {
                 return res.status(403).send({ message: 'Forbidden Access' })
             }
-        })
+        });
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
@@ -130,6 +131,12 @@ async function run() {
             }
             const result = await bookingCollection.insertOne(booking);
             return res.send({ success: true, result });
+        });
+
+        app.post('/doctor', async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result)
         })
 
     }
@@ -143,6 +150,8 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Hello From Doctor Bari!')
 });
+
+
 
 app.listen(port, () => {
     console.log(`Doctors app listening on port ${port}`)
